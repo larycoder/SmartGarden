2eee36069e317ef0e48d7d74aedcbafed7932c10
hducminh0 <hduc.minhlp@gmail.com>
2019-05-29 20:47:53 +0700

    Modify insert.php to insert into Senser_Reading instead of Value

--- system/server/insert.php
+++ system/server/insert.php
@@ -2,9 +2,9 @@
 	$response = array();
 	if (isset($_GET)) {
 		$Sensor_Index = $_GET['ss_index'];
-		$Value = $_GET['val'];	
+		$Sensor_Reading = $_GET['val'];	
 		echo $Sensor_Index;
-		echo $Value;
+		echo $Sensor_Reading;
 		
 		// include db_connect
 		$filepath = realpath(dirname(__FILE__));
@@ -15,7 +15,7 @@
 
 		// SQL query to insert data to table 
 		if ($db->connect()) {
-			$query_str = "INSERT INTO Data (Sensor_Index, Value) VALUES ($Sensor_Index, $Value)";
+			$query_str = "INSERT INTO Data (Sensor_Index, Sensor_Reading) VALUES ($Sensor_Index, $Sensor_Reading)";
 			$qresult = $db->query($query_str);
 
 			if (false === $qresult) {
